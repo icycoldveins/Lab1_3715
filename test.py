0954a96c2c31c3be3d8e41a8ec9756bea2b7b03a
icycoldveins <81425589+icycoldveins@users.noreply.github.com>
2023-01-31 02:57:07 -0500

    new stuff

--- test.py
+++ test.py
@@ -21,17 +21,9 @@ X2 = X[:, -2]
 # print("house2: {}".format(house2))
 # print("inner product: {}".format(np.inner(house1,house2)))
 
-# 1. addition and subtraction
-result_add = X1 + X2
-result_sub = X1 - X2
-# print(result_sub)
-# 2. element-wise multiplication
-result_mul = np.multiply(X1, X2)
-# 3. inner product
-result_inner = (np.inner(X1, X2))
-# 4. three norms
-l1_norm = np.linalg.norm(X1-X2, ord=1)
-l2_norm = np.linalg.norm(X1-X2, ord=2)
-linf_norm = np.linalg.norm(X1-X2, ord=np.inf)
-# 5. compute the distance between these two vectors
-distance = np.linalg.norm(X1 - X2, ord=2)
+
+
+A = X[:, :3]
+B = X[:, 3:]
+C=np.transpose(B)
+print(C.shape)
